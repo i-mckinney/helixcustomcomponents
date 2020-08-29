99d5bc10066487af10f87e976ebc22f62b1a817c
i-mckinney <baseballrocks@gmail.com>
2020-08-29 05:00:25 -0700

    Changelog for v.0.1.0

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,12 +1,12 @@
-import Header from './components/StorybookDocsExamples/Header';
-import Button from './components/StorybookDocsExamples/Button';
-import Page from './components/StorybookDocsExamples/Page'
+import { Header } from './components/StorybookDocsExamples/Header';
+import { Button } from './components/StorybookDocsExamples/Button';
+import { Page } from './components/StorybookDocsExamples/Page'
 import StyledButton from './components/MUI-Buttons/styled-button'
 
 export default Example;
 
 export {
-  SecondExample,
+  Header,
   Button,
   Page,
   StyledButton
