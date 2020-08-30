27ce6f5e77dc03658b9e320b2047498531131bfa
i-mckinney <baseballrocks@gmail.com>
2020-08-29 20:44:46 -0700

    removed src issue

--- .storybook/main.js
+++ .storybook/main.js
@@ -6,8 +6,8 @@ const npmLibConfig = require('../config/webpack.config')
 
 module.exports = {
   "stories": [
-    "../src/**/**/*.stories.mdx",
-    "../src/**/**/*.stories.@(js|jsx|ts|tsx)"
+    "../src/**/**/**/*.stories.mdx",
+    "../src/**/**/**/*.stories.@(js|jsx|ts|tsx)"
   ],
   "addons": [
     "@storybook/addon-links",
---
+++ src/index.js
@@ -0,0 +1,11 @@
+import { Header } from './lib/components/StorybookDocsExamples/Header';
+import { Button } from './lib/components/StorybookDocsExamples/Button';
+import { Page } from './lib/components/StorybookDocsExamples/Page'
+import StyledButton from './lib/components/MUI-Buttons/styled-button'
+
+export {
+  Header,
+  Button,
+  Page,
+  StyledButton
+};
